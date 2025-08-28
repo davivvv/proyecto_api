03236c1e1b239f8c0954c019c0489596c87819b1
davivvv <rodriguezdavidandres811@gmail.com>
2025-08-28 11:59:55 -0500

    Fix static files for Render

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = []
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
